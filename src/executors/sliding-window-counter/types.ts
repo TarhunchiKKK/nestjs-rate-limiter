22e0d265dff5ecfa887a737db7e2d23f07621f5e
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-25 09:46:53 +0300

    feat(core): types for sliding window counter strategy

---
+++ src/executors/sliding-window-counter/types.ts
@@ -0,0 +1,11 @@
+export type SlidingWindowCounterOptions = {
+    strategy: "sliding-window-counter"
+    limit: number;
+    windowMs: number;
+}
+
+export type SlidingWindowCounterState = {
+    currentWindowStart: number;
+    currentCount: number;
+    previousCont: number;
+}
