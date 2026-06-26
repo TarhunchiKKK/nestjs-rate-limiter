dd274b611f8bb8e0defc0c69ac54c0db492388cc
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-26 10:09:39 +0300

    feat(config): create `RateLimiterOptions` and `RateLimiterAsyncOptions` types

--- src/config/types/index.ts
+++ src/config/types/index.ts
@@ -1,4 +1,21 @@
-export type { CleanerOptions } from "./cleaner.options";
-export type { CustomProvidersOptions } from "./custom-providers.options";
-export type { ExecutorOptions } from "./executor.options";
-export type { KeyExtractorOptions } from "./key-extractors.options";
+import type { ModuleMetadata } from "@nestjs/common";
+import type { CleanerOptions } from "../../cleaners";
+import type { CustomProvidersOptions } from "./custom-providers.options";
+import type { ExecutorOptions } from "./executor.options";
+import type { KeyExtractorOptions } from "./key-extractors.options";
+
+export type RateLimiterOptions = {
+    limiter: ExecutorOptions;
+
+    cleaner?: CleanerOptions;
+
+    keyExtractor?: KeyExtractorOptions;
+
+    custom?: CustomProvidersOptions;
+};
+
+export type RateLimiterAsyncOptions = Pick<ModuleMetadata, "imports"> & {
+    inject: any[]
+
+    useFactory: (...args: any[]) => RateLimiterOptions | Promise<RateLimiterOptions>
+}
