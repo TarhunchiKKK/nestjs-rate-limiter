d93de18d569110c9bd318855745c2425752c8ac4
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-28 01:38:29 +0300

    feat(decorators): add options normalization to `RateLimit` decorator

--- src/decorators/rate-limit.decorator.ts
+++ src/decorators/rate-limit.decorator.ts
@@ -1,9 +1,10 @@
 import { applyDecorators } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
+import { normalizeOptions } from "../config/helpers";
 import type { RateLimitNormalizedOptions, RateLimitOptions } from "../config/options";
 
 export const RateLimitDecorator = Reflector.createDecorator<RateLimitNormalizedOptions>();
 
 export function RateLimit(options: RateLimitOptions) {
-    return applyDecorators(RateLimitDecorator(options ?? {}));
+    return applyDecorators(RateLimitDecorator(options ? normalizeOptions(options) : {}));
 }
