3c6f421a0bb0319cd3ed75b7482e79d153711af6
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-28 02:35:51 +0300

    feat: add providers discovery to `ProvidersDiscoveryService` provider

--- src/services/providers-discovery.service.ts
+++ src/services/providers-discovery.service.ts
@@ -1,21 +1,47 @@
-import { Inject, Injectable, type OnModuleInit, type Type } from "@nestjs/common";
+import { Inject, Injectable, type InjectionToken, type OnModuleInit } from "@nestjs/common";
 import { ModulesContainer } from "@nestjs/core";
 import type { ErrorFactoryFn, IErrorFactory } from "../custom/error-factories";
 import type { IKeyExtractor, KeyExtractorFn } from "../custom/key-extractors";
 import type { IOptionsFactory, OptionsFactoryFn } from "../custom/options-factories";
 import type { IExecutor } from "../executors";
-import type { TokenType } from "../shared/nestjs";
 
 @Injectable()
 export class ProvidersDiscoveryService implements OnModuleInit {
-    private readonly executorsMap: Map<Type<IExecutor<unknown>> | TokenType, IExecutor<unknown>>;
-    private readonly keyExtractorsMap: Map<Type<IKeyExtractor> | TokenType, KeyExtractorFn>;
-    private readonly errorFacotiesMap: Map<Type<IErrorFactory> | TokenType, ErrorFactoryFn>;
-    private readonly optionsFactoriesMap: Map<Type<IOptionsFactory> | TokenType, OptionsFactoryFn>;
+    private readonly executorsMap: Map<InjectionToken, IExecutor<unknown>>;
+    private readonly keyExtractorsMap: Map<InjectionToken, KeyExtractorFn>;
+    private readonly errorFactoriesMap: Map<InjectionToken, ErrorFactoryFn>;
+    private readonly optionsFactoriesMap: Map<InjectionToken, OptionsFactoryFn>;
 
     public constructor(@Inject(ModulesContainer) private readonly modulesContainer: ModulesContainer) {}
 
-    public onModuleInit() {}
+    public onModuleInit() {
+        for (const moduleInstance of this.modulesContainer.values()) {
+            for (const provider of moduleInstance.providers.values()) {
+                const token = provider.token;
+                const instance = provider.instance;
+
+                if (!instance) {
+                    continue;
+                }
+
+                if (this.isExecutor(instance)) {
+                    this.executorsMap.set(token, instance);
+                }
+
+                if (this.isKeyExtractor(instance)) {
+                    this.keyExtractorsMap.set(token, (context) => instance.extract(context));
+                }
+
+                if (this.isErrorFactory(instance)) {
+                    this.errorFactoriesMap.set(token, (context, options, key) => instance.getError(context, options, key));
+                }
+
+                if (this.isOptionsFactory(instance)) {
+                    this.optionsFactoriesMap.set(token, (context) => instance.getOptions(context));
+                }
+            }
+        }
+    }
 
     private isExecutor(provider: any) {
         const methodKey = "check" satisfies keyof IExecutor<unknown>;
@@ -29,7 +55,7 @@ export class ProvidersDiscoveryService implements OnModuleInit {
         return this.isValidProvider<IKeyExtractor>(provider, methodKey);
     }
 
-    private isErrorfatory(provider: any) {
+    private isErrorFactory(provider: any) {
         const methodKey = "getError" satisfies keyof IErrorFactory;
 
         return this.isValidProvider<IErrorFactory>(provider, methodKey);
