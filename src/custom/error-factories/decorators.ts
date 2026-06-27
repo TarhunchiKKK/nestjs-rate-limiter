a3e68467861e31e115542d274594925075ba888b
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-28 02:42:46 +0300

    feat(custom): create `ErrorFactory` decorator

---
+++ src/custom/error-factories/decorators.ts
@@ -0,0 +1,11 @@
+import { Injectable, type InjectableOptions } from "@nestjs/common";
+
+export const ERROR_FACTORY_METADATA = "__rate_limiter_error_factory__";
+
+export function ErrorFactory(options?: InjectableOptions): ClassDecorator {
+    return (target) => {
+        Injectable(options)(target);
+
+        Reflect.defineMetadata(ERROR_FACTORY_METADATA, true, target);
+    };
+}
