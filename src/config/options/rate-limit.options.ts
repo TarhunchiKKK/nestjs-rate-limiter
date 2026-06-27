7228b6ca4af62d3262c5c9621500495cb474fd58
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-27 20:21:04 +0300

    feat(config): create `RateLimitOptionsFactoryOptions` type

--- src/config/options/rate-limit.options.ts
+++ src/config/options/rate-limit.options.ts
@@ -1,11 +1,13 @@
 import type { Scope, Type } from "@nestjs/common";
 import type { ErrorFactoryFn, IErrorFactory } from "../../custom/error-factories";
 import type { IKeyExtractor, KeyExtractorFn } from "../../custom/key-extractors";
+import type { IOptionsFactory, OptionsFactoryFn } from "../../custom/options-factories";
 import type { StrategyOptionsUnion } from "../../executors";
 import type { TokenType } from "../../shared/nestjs";
+import type { PartialUnionMembers } from "../../shared/ts";
 
 export type RateLimitBaseOptions = {
-    scope: Scope;
+    scope?: Scope;
 };
 
 export type RateLimitKeyExtractorOptions =
@@ -16,4 +18,14 @@ export type RateLimitErrorFactoryOptions =
     | { errorFactory: Type<IErrorFactory> | TokenType; errorFactoryFn?: never }
     | { errorFactory?: never; errorFactoryFn: ErrorFactoryFn };
 
-export type RateLimitOptions = RateLimitBaseOptions & RateLimitKeyExtractorOptions & RateLimitErrorFactoryOptions & StrategyOptionsUnion;
+export type RateLimitOptionsFactoryOptions =
+    | { factory?: Type<IOptionsFactory> | TokenType; optionsFactoryFn?: never }
+    | { factory?: never; optionsFactoryFn?: OptionsFactoryFn };
+
+export type RateLimitStrategyOptions = PartialUnionMembers<StrategyOptionsUnion>;
+
+export type RateLimitOptions = RateLimitBaseOptions &
+    RateLimitKeyExtractorOptions &
+    RateLimitErrorFactoryOptions &
+    RateLimitOptionsFactoryOptions &
+    RateLimitStrategyOptions;
