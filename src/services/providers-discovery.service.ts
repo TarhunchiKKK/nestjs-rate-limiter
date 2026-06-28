48cf6db1a8185c397d10f8b5bb12be920a650bda
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-28 03:13:43 +0300

    feat: add discovery methods for `ProvidersDiscoveryService` provider

--- src/services/providers-discovery.service.ts
+++ src/services/providers-discovery.service.ts
@@ -3,19 +3,60 @@ import { ModulesContainer, Reflector } from "@nestjs/core";
 import { ERROR_FACTORY_METADATA, type ErrorFactoryFn, type IErrorFactory } from "../custom/error-factories";
 import { type IKeyExtractor, KEY_EXTRACTOR_METADATA, type KeyExtractorFn } from "../custom/key-extractors";
 import { type IOptionsFactory, OPTIONS_FACTORY_METADATA, type OptionsFactoryFn } from "../custom/options-factories";
-import { EXECUTOR_METADATA_KEY, type IExecutor } from "../executors";
+import { type AllStrategiesOptions, EXECUTOR_METADATA_KEY, type ExecutorMetadata, type IExecutor, type StrategiesRenamingMap } from "../executors";
+import type { Strategies } from "../shared/types";
 
 @Injectable()
 export class ProvidersDiscoveryService implements OnModuleInit {
-    private readonly executorsMap: Map<InjectionToken, IExecutor<unknown>>;
-    private readonly keyExtractorsMap: Map<InjectionToken, KeyExtractorFn>;
-    private readonly errorFactoriesMap: Map<InjectionToken, ErrorFactoryFn>;
-    private readonly optionsFactoriesMap: Map<InjectionToken, OptionsFactoryFn>;
+    private readonly executorsMap = new Map<Strategies, IExecutor<unknown>>();
+    private readonly keyExtractorsMap = new Map<InjectionToken, KeyExtractorFn>();
+    private readonly errorFactoriesMap = new Map<InjectionToken, ErrorFactoryFn>();
+    private readonly optionsFactoriesMap = new Map<InjectionToken, OptionsFactoryFn>();
 
     public constructor(
         @Inject(ModulesContainer) private readonly modulesContainer: ModulesContainer,
         @Inject(Reflector) private readonly reflector: Reflector
     ) {}
+
+    public getExecutor<Strategy extends Strategies>(strategy: Strategy) {
+        const executor = this.executorsMap.get(strategy);
+
+        if (!executor) {
+            throw new Error(`No executor found for strategy: "${strategy}""`);
+        }
+
+        return executor as IExecutor<AllStrategiesOptions[(typeof StrategiesRenamingMap)[Strategy]]>;
+    }
+
+    public getKeyExtractor(token: InjectionToken) {
+        const keyExtractor = this.keyExtractorsMap.get(token);
+
+        if (!keyExtractor) {
+            throw new Error(`No key extractor found for token: ${String(token)}`);
+        }
+
+        return keyExtractor;
+    }
+
+    public getErrorFactory(token: InjectionToken) {
+        const errorFactory = this.errorFactoriesMap.get(token);
+
+        if (!errorFactory) {
+            throw new Error(`No error factory found for token: ${String(token)}`);
+        }
+
+        return errorFactory;
+    }
+
+    public getOptionsFactory(token: InjectionToken) {
+        const optionsFactory = this.optionsFactoriesMap.get(token);
+
+        if (!optionsFactory) {
+            throw new Error(`No options factory found for token: ${String(token)}`);
+        }
+
+        return optionsFactory;
+    }
 
     public onModuleInit() {
         for (const moduleInstance of this.modulesContainer.values()) {
@@ -28,7 +69,9 @@ export class ProvidersDiscoveryService implements OnModuleInit {
                 }
 
                 if (this.isValidProvider<IExecutor<unknown>>(instance, "check", EXECUTOR_METADATA_KEY)) {
-                    this.executorsMap.set(token, instance);
+                    const metadata = this.reflector.get<ExecutorMetadata>(EXECUTOR_METADATA_KEY, instance.constructor);
+
+                    this.executorsMap.set(metadata.strategy, instance);
                 }
 
                 if (this.isValidProvider<IKeyExtractor>(instance, "extract", KEY_EXTRACTOR_METADATA)) {
