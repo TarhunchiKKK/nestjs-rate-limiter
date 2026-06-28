db178813e84c7fc42da0e9b23ec4723efba43b30
TarhunchiKKK <kostabarilo12@gmail.com>
2026-06-28 14:16:01 +0300

    fix(executors): fix `AllStrategiesOptions` type

--- src/config/options/rate-limiter-module.options.ts
+++ src/config/options/rate-limiter-module.options.ts
@@ -3,8 +3,19 @@ import type { ModuleMetadata, Provider, Type } from "@nestjs/common";
 import type { IErrorFactory } from "../../custom/error-factories";
 import type { IKeyExtractor } from "../../custom/key-extractors";
 import type { IOptionsFactory, OptionsFactoryFn } from "../../custom/options-factories";
-import type { DeepRequired, FlattenOptionalNeverUnion, TokenType } from "../../shared/lib";
+import type { DeepPartial, DeepRequired, FlattenOptionalNeverUnion, TokenType } from "../../shared/lib";
 import type { BaseOptions, ErrorFactoryOptions, KeyExtractorOptions, StorageOptions, StrategyOptions } from "./common.options";
+import type { AllStrategiesOptions } from "../../executors";
+
+type ModuleStrategyOptions = Pick<StrategyOptions, "strategy"> & {
+    strategyOptions: {
+        fixedWindow: AllStrategiesOptions["fixed-window"];
+        tokenBucket: AllStrategiesOptions["token-bucket"];
+        slidingWindowCounter: AllStrategiesOptions["sliding-window-counter"];
+        slidingWindowLog: AllStrategiesOptions["sliding-window-log"];
+        leakyBucket: AllStrategiesOptions["leakey-bucket"];
+    };
+};
 
 type ModuleOptionsFactoryOptions =
     | { optionsFactory: Type<IOptionsFactory> | TokenType; optionsFactoryFn?: never }
@@ -19,11 +30,11 @@ type CustomProvidersOptions = {
 };
 
 export type RateLimiterModuleOptions = Partial<BaseOptions> &
+    StorageOptions &
+    DeepPartial<ModuleStrategyOptions> &
     Partial<KeyExtractorOptions> &
     Partial<ErrorFactoryOptions> &
     Partial<ModuleOptionsFactoryOptions> &
-    Partial<StrategyOptions> &
-    StorageOptions &
     CustomProvidersOptions;
 
 export type RateLimiterModuleAsyncOptions = Pick<ModuleMetadata, "imports"> & {
@@ -33,7 +44,7 @@ export type RateLimiterModuleAsyncOptions = Pick<ModuleMetadata, "imports"> & {
 
 export type RateLimiterModuleFullOptions = BaseOptions &
     StorageOptions &
-    StrategyOptions &
+    ModuleStrategyOptions &
     KeyExtractorOptions &
     ErrorFactoryOptions &
     Partial<FlattenOptionalNeverUnion<ModuleOptionsFactoryOptions>> &
