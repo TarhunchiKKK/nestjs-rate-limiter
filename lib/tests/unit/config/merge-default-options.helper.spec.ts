4f75cd2e101557b72cb1767207cc9efdf43b4f0e
TarhunchiKKK <kostabarilo12@gmail.com>
2026-07-13 23:10:11 +0300

    tests(config); unit-test cases for `mergeDefaultOptions` helper with different strategies and strategy options

--- lib/tests/unit/config/merge-default-options.helper.spec.ts
+++ lib/tests/unit/config/merge-default-options.helper.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect } from "bun:test";
 import { it } from "node:test";
 import type { RateLimiterModuleOptions } from "../../../src";
 import { mergeDefaultOptions } from "../../../src/config/defaults";
+import { RATE_LIMITER_MODULE_DEFAULT_OPTIONS } from "../../../src/config/defaults/default-options.constants";
 import { DEFAULT_SCOPE } from "../../../src/shared/model";
 
 describe("mergeDefaultOptions", () => {
@@ -56,11 +57,62 @@ describe("mergeDefaultOptions", () => {
     });
 
     describe("strategy", () => {
-        it("default", () => {});
+        it("default", () => {
+            const input = {
+                storage: "in-memory"
+            } satisfies RateLimiterModuleOptions;
 
-        it("custom", () => {});
+            const result = mergeDefaultOptions(input);
 
-        it("strategy options", () => {});
+            expect(result.strategy).toBe(RATE_LIMITER_MODULE_DEFAULT_OPTIONS.strategy);
+        });
+
+        it("custom", () => {
+            const input = {
+                storage: "in-memory",
+                strategy: "sliding-window-log"
+            } satisfies RateLimiterModuleOptions;
+
+            const result = mergeDefaultOptions(input);
+
+            expect(result.strategy).toBe(input.strategy);
+        });
+
+        it("strategy options", () => {
+            const input = {
+                storage: "in-memory",
+                strategyOptions: {
+                    fixedWindow: {
+                        // full strategy options
+                        ttl: 10,
+                        limit: 10
+                    },
+                    tokenBucket: {
+                        // partial options
+                        ttl: 10
+                    },
+                    slidingWindowCounter: {
+                        // empty options
+                    }
+                    // no options for remaining strategies
+                }
+            } satisfies RateLimiterModuleOptions;
+
+            const result = mergeDefaultOptions(input);
+
+            // expectations for full options object
+            expect(result.strategyOptions.fixedWindow).toEqual(input.strategyOptions.fixedWindow);
+
+            // expectations for partial options object
+            expect(result.strategyOptions.tokenBucket.ttl).toBe(input.strategyOptions.tokenBucket.ttl);
+            expect(result.strategyOptions.tokenBucket.refillRate).toBe(RATE_LIMITER_MODULE_DEFAULT_OPTIONS.strategyOptions.tokenBucket.refillRate);
+            expect(result.strategyOptions.tokenBucket.capacity).toBe(RATE_LIMITER_MODULE_DEFAULT_OPTIONS.strategyOptions.tokenBucket.capacity);
+
+            // expectations for empty options objects
+            expect(result.strategyOptions.slidingWindowCounter).toEqual(RATE_LIMITER_MODULE_DEFAULT_OPTIONS.strategyOptions.slidingWindowCounter);
+            expect(result.strategyOptions.slidingWindowLog).toEqual(RATE_LIMITER_MODULE_DEFAULT_OPTIONS.strategyOptions.slidingWindowLog);
+            expect(result.strategyOptions.leakyBucket).toEqual(RATE_LIMITER_MODULE_DEFAULT_OPTIONS.strategyOptions.leakyBucket);
+        });
     });
 
     describe("providers", () => {
